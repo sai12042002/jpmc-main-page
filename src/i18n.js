61864cfae21499177f1d7d03da733d5f27895bcb
sai12042002 <sai12042002@gmail.com>
2022-06-26 06:15:15 +0530

    commit to change origin

--- src/i18n.js
+++ src/i18n.js
@@ -2,9 +2,10 @@ import i18next from "i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+// require ('dotenv').config()
 
 const apiKey = "Xuv_g3XXVHpiZSGPMyxQzw";
-const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
+const loadPath =`https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
 
 i18next
   .use(HttpBackend)
